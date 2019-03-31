1d2a8ea3efb1ba0c4079d6cf0de2d7f26b450080
Eric <eric_gomez_1@hotmail.com>
2019-03-30 21:14:21 -0700

    added visit controller and mappings. closes #57

---
+++ pet-clinic-web/src/test/java/app/petclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,35 @@
+package app.petclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitControllerTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @Test
+    void setAllowedFields()
+    {
+    }
+
+    @Test
+    void loadPetWithVisit()
+    {
+    }
+
+    @Test
+    void initNewVisitForm()
+    {
+    }
+
+    @Test
+    void processNewVisitForm()
+    {
+    }
+}
